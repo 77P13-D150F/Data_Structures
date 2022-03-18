c1944da9af0ea184454d96f52fffe329350b2d9b
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-03-18 11:48:47 +0100

    Update Queue.py

--- Queue.py
+++ Queue.py
@@ -6,6 +6,7 @@ class Node:
     def __init__(self, data=None):
         self.data = data
         self.next = None
+        self.previous = None
     
     def __repr__(self):
         return self.data
@@ -16,26 +17,28 @@ class Queue:
         self.head = None
         self.tail = None
         self.size = 0
-        
+
     def enqueue(self, data):
         node = Node(data)
         if self.head is None:
             self.head = node
             self.tail = node
         else:
-            self.tail.next = node
-            self.tail = self.tail.next
+            node.next = self.head
+            self.head.previous = node
+            self.head = self.head.previous
         self.size += 1
-
+            
     def dequeue(self):
         if self.head is not None:
-            data = self.head
-            self.head = self.head.next
+            data = self.tail
+            self.tail = self.tail.previous
+            self.tail.next = None
             self.size -= 1
             return data
         else:
             return None
-        
+
     def flush(self):
         self.head = None
         self.tail = None
