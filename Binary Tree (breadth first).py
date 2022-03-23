627c0804d67dfa7029b21ac8b272022b48cf9568
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-03-23 11:53:20 +0100

    Update Binary Tree (breadth first).py

--- Binary Tree (breadth first).py
+++ Binary Tree (breadth first).py
@@ -98,15 +98,16 @@ class BinaryTree:
             if node.right:
                 queue.append(node.right)
         queue = deque([subtree_root])
+        list_nodes = []
         while len(queue) > 0:
             node = queue.popleft()
-            yield node.data
+            list_nodes.append(node.data)
             self.size -= 1
             if node.left:
                 queue.append(node.left)
             if node.right:
                 queue.append(node.right)   
-
+        return list_nodes
 
     def display_leaves(self):
         leaves = []
@@ -122,24 +123,27 @@ class BinaryTree:
         print(leaves)
     
     def pop_leaf(self, data):
-        queue = deque([self.root])
-        while len(queue) > 0:
-            node = queue.popleft()
-            if node.left.data == data and (node.left.left is None) and (node.left.right is None):
-                leaf = node.left.data
-                node.left = None
-                self.size -= 1
-                return leaf
-            if node.right.data == data and (node.right.left is None) and (node.right.right is None):
-                leaf = node.right.data
-                node.right = None
-                self.size -= 1
-                return leaf
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-    
+        if self.search(data):
+            queue = deque([self.root])
+            while len(queue) > 0:
+                node = queue.popleft()
+                if node.left.data == data and (node.left.left is None) and (node.left.right is None):
+                    leaf = node.left.data
+                    node.left = None
+                    self.size -= 1
+                    return leaf
+                if node.right.data == data and (node.right.left is None) and (node.right.right is None):
+                    leaf = node.right.data
+                    node.right = None
+                    self.size -= 1
+                    return leaf
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+        else:
+            return None
+        
     def min_node(self):
         current = self.root
         while current.left is not None:
@@ -170,7 +174,45 @@ class BinaryTree:
                     c = 0
         else:
             return None
-        
+
+    def height(self):
+        queue = deque([self.root])
+        depth = 0
+        c = 0
+        heights = [] 
+        while len(queue) > 0:
+            layer = 2 ** depth
+            node = queue.popleft()
+            heights.append(depth)
+            c += 1
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            if c == layer:
+                depth += 1
+                c = 0
+        return max(heights)
+
+    def width(self):
+        queue = deque([self.root])
+        depth = 0
+        c = 0
+        width = []
+        while len(queue) > 0:
+            layer = 2 ** depth
+            node = queue.popleft()
+            c += 1
+            width.append(c)
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+            if c == layer:
+                depth += 1
+                c = 0
+        return max(width)
+    
     def flush(self):
         self.root = None
         self.size = 0
