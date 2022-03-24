1f4ddb5c3e998a5a22295bc3ae287e516e5231a5
77P13-D150F <13771688+77P13-D150F@users.noreply.github.com>
2022-03-24 15:28:27 +0100

    Update Binary Tree.py

--- Binary Tree.py
+++ Binary Tree.py
@@ -60,12 +60,87 @@ class BinaryTree:
                         parent.right = node
                         break
             self.size += 1
+
+    # the pop process is different depending on how many child nodes the target has
+    def BST_pop_node(self, data):
+        if self.search(data):
+            current = self.root
+            if data == current.data:
+                if current.left is None and current.right is None:
+                    self.root = None
+                elif current.left is None:
+                    self.root = current.right
+                elif current.right is None:
+                    self.root = current.left
+                else:
+                    current = current.right
+                    if current.left is None:
+                        current.left = self.root.left
+                        self.root = current
+                    else:
+                        while current.left is not None:
+                            parent = current
+                            current = current.left
+                        current.left = self.root.left
+                        current.right = parent
+                        parent.left = None
+                        self.root = current
+            else:
+                while True:
+                    parent = current
+                    if data < parent.data:
+                        current = current.left
+                        if data == current.data:
+                            if current.left is None and current.right is None:
+                                parent.left = None
+                            elif current.left is None:
+                                parent.left = current.right
+                            elif current.right is None:
+                                parent.left = current.left
+                            else:
+                                if current.left is None:
+                                    current.left = parent.left
+                                    parent = current
+                                else:
+                                    root = parent
+                                    while current.left is not None:
+                                        parent = current
+                                        current = current.left
+                                    current.right = parent.right
+                                    parent.left = None
+                                    root.right = current
+                            break
+                    else:
+                        current = current.right
+                        if data == current.data:
+                            if current.left is None and current.right is None:
+                                parent.right = None
+                            elif current.left is None:
+                                parent.right = current.right
+                            elif current.right is None:
+                                parent.right = current.left
+                            else:
+                                if current.left is None:
+                                    current.left = parent.left
+                                    parent = current
+                                else:
+                                    root = parent
+                                    while current.left is not None:
+                                        parent = current
+                                        current = current.left
+                                    current.right = parent.right
+                                    parent.left = None
+                                    root.right = current
+                            break
+                self.size -= 1
+                return data
             
     def display(self):
-        print(f'In-order (Depth first): {self.inorder(self.root)}')
-        print(f'Pre-order (Depth first): {self.preorder(self.root)}')
-        print(f'Post-order (Depth first): {self.postorder(self.root)}')
-        print(f'Breadth first: {self.breadth_first()}')
+        if self.root is not None:
+            print(f'In-order (Depth first): {self.inorder(self.root)}')
+            print(f'Pre-order (Depth first): {self.preorder(self.root)}')
+            print(f'Post-order (Depth first): {self.postorder(self.root)}')
+            print(f'Breadth first: {self.breadth_first()}')
     
     # child left -> parent -> child right
     def inorder(self, node, results=[]):
@@ -109,81 +184,85 @@ class BinaryTree:
 
     # Breadth First tree search
     def search(self, data):
-        queue = deque([self.root])
-        while len(queue) > 0:
-            node = queue.popleft()
-            if node.data == data:
-                return True
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-        return False
-
-    # Full sub-tree display, using Breadth First search and traversal
-    def display_subtree(self, data):
-        queue = deque([self.root])
-        while len(queue) > 0:
-            node = queue.popleft()
-            if node.data != data:
+        if self.root is not None:
+            queue = deque([self.root])
+            while len(queue) > 0:
+                node = queue.popleft()
+                if node.data == data:
+                    return True
                 if node.left:
                     queue.append(node.left)
                 if node.right:
                     queue.append(node.right)
-            else:
-                nodes = []
-                queue = deque([node])
-                while len(queue) > 0:
-                    node = queue.popleft()
-                    nodes.append(node.data)
+            return False
+
+    # Full sub-tree display, using Breadth First search and traversal
+    def display_subtree(self, data):
+        if self.root is not None:
+            queue = deque([self.root])
+            while len(queue) > 0:
+                node = queue.popleft()
+                if node.data != data:
                     if node.left:
                         queue.append(node.left)
                     if node.right:
                         queue.append(node.right)
-                print(nodes)
+                else:
+                    nodes = []
+                    queue = deque([node])
+                    while len(queue) > 0:
+                        node = queue.popleft()
+                        nodes.append(node.data)
+                        if node.left:
+                            queue.append(node.left)
+                        if node.right:
+                            queue.append(node.right)
+                    print(nodes)
 
     # Full sub-tree delete, using Breadth First search and traversal            
     def pop_subtree(self, data):
-        queue = deque([self.root])
-        while len(queue) > 0:
-            node = queue.popleft()
-            if node.left.data == data:
-                subtree_root = node.left
-                node.left = None
-                break
-            if node.right.data == data:
-                subtree_root = node.right
-                node.right = None
-                break
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-        queue = deque([subtree_root])
-        nodes = []
-        while len(queue) > 0:
-            node = queue.popleft()
-            nodes.append(node.data)
-            self.size -= 1
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)   
-        return nodes
+        if self.root is not None:
+            queue = deque([self.root])
+            while len(queue) > 0:
+                node = queue.popleft()
+                if node.left.data == data:
+                    subtree_root = node.left
+                    node.left = None
+                    break
+                if node.right.data == data:
+                    subtree_root = node.right
+                    node.right = None
+                    break
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            queue = deque([subtree_root])
+            nodes = []
+            while len(queue) > 0:
+                node = queue.popleft()
+                nodes.append(node.data)
+                self.size -= 1
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)   
+            return nodes
 
     # it relies on Breadth First traversal
     def display_leaves(self):
-        leaves = []
-        queue = deque([self.root])
-        while len(queue) > 0:
-            node = queue.popleft()
-            if (node.left is None) and (node.right is None):
-                leaves.append(node.data)
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-        print(leaves)
+        if self.root is not None:
+            leaves = []
+            queue = deque([self.root])
+            while len(queue) > 0:
+                node = queue.popleft()
+                if (node.left is None) and (node.right is None):
+                    leaves.append(node.data)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            print(leaves)
     
     # it relies on Breadth First traversal
     def pop_leaf(self, data):
@@ -210,17 +289,19 @@ class BinaryTree:
     
     # the furthest bottom-left node
     def min_node(self):
-        node = self.root
-        while node.left is not None:
-            node = node.left
-        return node.data
+        if self.root is not None:
+            node = self.root
+            while node.left is not None:
+                node = node.left
+            return node.data
 
     # the furthest bottom-right node
     def max_node(self):
-        node = self.root
-        while node.right is not None:
-            node = node.right
-        return node.data
+        if self.root is not None:
+            node = self.root
+            while node.right is not None:
+                node = node.right
+            return node.data
 
     # display the level value of a node
     def level_node(self, data):
@@ -244,43 +325,45 @@ class BinaryTree:
 
     # display the heigth of the tree
     def height(self):
-        queue = deque([self.root])
-        depth = 0
-        c = 0
-        heights = [] 
-        while len(queue) > 0:
-            layer = 2 ** depth
-            node = queue.popleft()
-            heights.append(depth)
-            c += 1
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-            if c == layer:
-                depth += 1
-                c = 0
-        return max(heights)
+        if self.root is not None:
+            queue = deque([self.root])
+            depth = 0
+            c = 0
+            heights = [] 
+            while len(queue) > 0:
+                layer = 2 ** depth
+                node = queue.popleft()
+                heights.append(depth)
+                c += 1
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                if c == layer:
+                    depth += 1
+                    c = 0
+            return max(heights)
 
     # display the width of the tree
     def width(self):
-        queue = deque([self.root])
-        depth = 0
-        c = 0
-        width = []
-        while len(queue) > 0:
-            layer = 2 ** depth
-            node = queue.popleft()
-            c += 1
-            width.append(c)
-            if node.left:
-                queue.append(node.left)
-            if node.right:
-                queue.append(node.right)
-            if c == layer:
-                depth += 1
-                c = 0
-        return max(width)
+        if self.root is not None:
+            queue = deque([self.root])
+            depth = 0
+            c = 0
+            width = []
+            while len(queue) > 0:
+                layer = 2 ** depth
+                node = queue.popleft()
+                c += 1
+                width.append(c)
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+                if c == layer:
+                    depth += 1
+                    c = 0
+            return max(width)
     
     # delete all the nodes of the whole tree
     def flush(self):
